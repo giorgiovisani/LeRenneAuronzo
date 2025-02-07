285324b63654f5734177797a1448613d9670f33f
giovisani <giorgio.visani@leitha.eu>
2025-02-07 01:23:34 +0100

    Rescaled navbar for mobile

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,8 @@
 import { useTranslation } from 'react-i18next';
 import { useState } from 'react';
 import { HiOutlineMenuAlt3, HiX } from "react-icons/hi"; // Menu icons
-import LanguageSwitcher from "./LanguageSwitcher"; // Import Language Switcher Component
+import LanguageSwitcher from "./LanguageSwitcher";
+import {CgMenuBoxed} from "react-icons/cg"; // Import Language Switcher Component
 
 interface NavBarProps {
   basename: string;
@@ -19,19 +20,22 @@ export const NavBar: React.FC<NavBarProps> = ({ basename, handleBookingClick }) 
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-16">
         {/* Navbar Brand */}
         <div className="flex items-center">
-          <a href={`${basename}`} className="text-xl font-bold text-gray-300 hover:text-white hover:font-bold no-underline">
+          <a
+            href={`${basename}`}
+            className="text-lg md:text-xl lg:text-2xl font-bold text-gray-300 hover:text-white hover:font-bold no-underline"
+          >
             {t('navbar.title')}
           </a>
         </div>
 
         {/* Desktop Navigation + Language Switcher */}
-        <div className="hidden md:flex items-center space-x-8">
-          {navbarItems.map((item, index) => (
+        <div className="hidden md:flex items-center space-x-4 lg:space-x-8">
+          {navbarItems.map((item, index) =>
             item === 'availability' ? (
               <button
                 key={index}
                 onClick={handleBookingClick}
-                className="text-gray-300 hover:text-white hover:font-bold font-medium no-underline text-xl p-2 m-0 leading-none bg-transparent border-none focus:outline-none"
+                className="text-gray-300 hover:text-white hover:font-bold font-medium no-underline text-sm md:text-xl p-1 md:p-2 m-0 leading-none bg-transparent border-none focus:outline-none"
               >
                 {t(`navbar.${item}`)}
               </button>
@@ -39,35 +43,33 @@ export const NavBar: React.FC<NavBarProps> = ({ basename, handleBookingClick }) 
               <a
                 key={index}
                 href={`${basename}#/${item}`}
-                className="text-gray-300 hover:text-white hover:font-bold font-medium no-underline text-xl p-2 m-0 leading-none"
+                className="text-gray-300 hover:text-white hover:font-bold font-medium no-underline text-sm md:text-xl p-1 md:p-2 m-0 leading-none"
               >
                 {t(`navbar.${item}`)}
               </a>
             )
-          ))}
-
-          {/* Language Switcher for Desktop */}
+          )}
+          {/* Desktop Language Switcher */}
           <LanguageSwitcher />
         </div>
 
         {/* Mobile Navbar & Language Switcher */}
-        <div className="flex md:hidden items-center space-x-4">
+        <div className="flex md:hidden items-center space-x-2">
           {/* Mobile Menu Button */}
           <button
             onClick={() => setIsOpen(!isOpen)}
-            className="bg-gray-800 p-3 rounded-full text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 transition-all transform hover:scale-110 shadow-lg"
+            className="bg-gray-800 p-2 md:p-3 rounded-full text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 transition-all transform hover:scale-110 shadow-lg"
             aria-controls="mobile-menu"
             aria-expanded={isOpen}
           >
             <span className="sr-only">Toggle navigation menu</span>
             {isOpen ? (
-              <HiX className="w-7 h-7 transition-all duration-300" />
+              <HiX className="w-6 h-6 md:w-7 md:h-7 transition-all duration-300" />
             ) : (
-              <HiOutlineMenuAlt3 className="w-7 h-7 transition-all duration-300" />
+              <CgMenuBoxed className="w-6 h-6 md:w-7 md:h-7 transition-all duration-300" />
             )}
           </button>
-
-          {/* Language Switcher for Mobile */}
+          {/* Mobile Language Switcher */}
           <LanguageSwitcher />
         </div>
       </div>
@@ -76,8 +78,11 @@ export const NavBar: React.FC<NavBarProps> = ({ basename, handleBookingClick }) 
       <div className={`${isOpen ? 'block' : 'hidden'} md:hidden`} id="mobile-menu">
         <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
           {navbarItems.map((item, index) => (
-            <a key={index} href={`${basename}#/${item}`}
-              className="text-gray-300 hover:text-white hover:font-bold block font-medium no-underline">
+            <a
+              key={index}
+              href={`${basename}#/${item}`}
+              className="text-gray-300 hover:text-white hover:font-bold block font-medium no-underline text-base p-1"
+            >
               {t(`navbar.${item}`)}
             </a>
           ))}
