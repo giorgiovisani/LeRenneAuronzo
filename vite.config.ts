ba9725975c7b8c79cdeaff1fb88a62ec04fc516f
giovisani <giorgio.visani@leitha.eu>
2025-05-28 00:31:54 +0200

    test without bsse (hoping vercel recognizes the app)

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/LeRenneAuronzo/',  // Make sure this matches your repository name //DA NON USARE, ROMPE TUTTO!!
+    // base: '/LeRenneAuronzo/',  // Make sure this matches your repository name //DA NON USARE, ROMPE TUTTO!!
     plugins: [react()],
 })
